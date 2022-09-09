77a7ee9523076249c92f170dc6a586da35b68b69
mshige1979 <mshige1979@ryuouen.net>
2022-09-10 08:39:04 +0900

    app1

---
+++ app1/preload.js
@@ -0,0 +1,12 @@
+// All the Node.js APIs are available in the preload process.
+// Chrome 拡張機能と同じサンドボックスも持っています。
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
